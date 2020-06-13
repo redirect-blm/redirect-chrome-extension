39a04c3d366cd0bfa62d9291b20f184081ea504b
mjr68 <matthewrose1799@gmail.com>
2020-06-13 19:11:37 -0400

    added spinner to ethics class while waiting for async api call

--- src/components/EthicsCard/EthicsCard.js
+++ src/components/EthicsCard/EthicsCard.js
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
+import Spinner from '../LoadSpinner/LoadSpinner';
 // import Axios from 'axios';
 
 export default function EthicsCard() {
-  //   const [company, setCompany] = useState();
-  //   useEffect(() => {
-  //     Axios.get('API call').then(res => {
-  //       setCompany(res.data);
-  //     });
-  //   });
+  const [company, setCompany] = useState(null);
+  useEffect(() => {
+    //     Axios.get('API call').then(res => {
+    //       setCompany(res.data);
+    //     });
+    var temp = {
+      title: 'Amazon',
+      description: 'Overview of ethics statement.'
+    };
+    setCompany(temp);
+  }, []);
 
-  var temp = {
-    title: 'Amazon',
-    description: 'Overview of ethics statement.'
-  };
+  let ethics =
+    company === null ? (
+      <Spinner />
+    ) : (
+      <Card className="mt-3 mb-3">
+        <Card.Body>
+          <Card.Title>{company.title}</Card.Title>
+          <Card.Text>{company.description}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
 
   return (
     <div className="text-center">
-      <h4>{temp.title}'s Ethics</h4>
-      <Card className="mt-3 mb-3">
-        <Card.Body>
-          <Card.Title>{temp.title}</Card.Title>
-          <Card.Text>{temp.description}</Card.Text>
-        </Card.Body>
-      </Card>
+      <h4>This Brand's Ethics</h4>
+      {ethics}
     </div>
   );
 }
