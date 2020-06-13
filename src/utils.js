8399bc224f3f5c8b1b337bf21260cec3eda505d9
Nicolas Ries <nries1@gmail.com>
2020-06-13 18:16:45 -0400

    exposed a class for reading data about amazon's DOM

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,43 @@
-const getBrands = () => {
-
+class Scraper {
+    constructor(document) {
+        this.document = document
+    }
+    get page() {
+        const title = this.document.getElementsByTagName('title')[0].innerText.replace(/Amazon.com: /, '')
+        return /Online Shopping for/.test(title) ? 'Home' : title
+    }
+    get category() {
+        const dropDown = document.getElementById('searchDropdownBox')
+        if (!dropDown) return null
+        const options = dropDown.getElementsByTagName('option');
+        const selectedIndex = dropDown.selectedIndex;
+        return !!selectedIndex && options[selectedIndex].innerText
+    }
+    get brands() {
+        const { document } = this;
+      // some pages have a list of brands in a descriptive div
+      // the brand div contains a UL and each list item has an id equal to something like p_89/Nike (p_###/<productname>)
+        const easyBrandItems = !!document.getElementById('brandsRefinements') && document.getElementById('brandsRefinements').getElementsByTagName('li')
+        if (easyBrandItems) return [...brandItems].map(({ innerText }) => innerText.replace(/\w_\d.\//,""))
+      // some pages have a a list of brands under a div with no id, where each brand is in an LI
+      // the brand name is contained in the aria-label property of the li element
+        const ariaLabelBrandItems = [...document.getElementsByTagName('li')].filter(_l => _l.ariaLabel)
+        if (ariaLabelBrandItems.length) return ariaLabelBrandItems.map(_l => _l.ariaLabel)
+      // some pages have brands as a list of anchor tags under a h3 with no id or class (xD?)
+        const headers = document.getElementsByTagName('h3')
+        const brandHeader = [...headers].filter(_h => _h.innerText === 'Featured Brands')[0]
+        const anchorBrandList = !!brandHeader && [...brandHeader.nextSibling.getElementsByTagName('a')].map(_a => _a.innerText)
+        if (anchorBrandList) return anchorBrandList;
+        return null
+    }
+    // use this to get the user's search term
+    get searchTerm() {
+        const { document } = this;
+        const input = document.getElementById('twotabsearchtextbox')
+        return !!input && input.value;
+    }
 }
 
 export default {
-    getBrands
+    Scraper
 }
