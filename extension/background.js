cf8c90287ffb46021908156e2a0140fc7af03318
Nicolas Ries <nries1@gmail.com>
2020-06-13 15:04:26 -0400

    style tweaks to popup

--- extension/background.js
+++ extension/background.js
@@ -2,10 +2,6 @@
 
 // Kept this code for reference, although I don't think we need storage.
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
-    console.log(`The color is green.`);
-  });
-
   // Show the page action (popup.html) when the URL host demain contains amazon
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
@@ -20,7 +16,3 @@ chrome.runtime.onInstalled.addListener(function () {
     ]);
   });
 });
-
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
