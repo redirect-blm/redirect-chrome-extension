147933d5259617b5e0d6abd287f96c96fb1af878
jngong <34215359+jngong@users.noreply.github.com>
2020-06-13 10:28:49 -0400

    added comments

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 "use strict";
 
-// Kept this code for reference
+// Kept this code for reference, although I don't think we need storage.
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ color: "#3aa757" }, function () {
     console.log(`The color is green.`);
   });
 
+  // Show the page action (popup.html) when the URL host demain contains amazon
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
@@ -19,7 +20,6 @@ chrome.runtime.onInstalled.addListener(function () {
     ]);
   });
 });
-
 
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
