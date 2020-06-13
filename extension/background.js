763ef6daeea6ab9491360a3e4b7f0b33153b34bd
jngong <34215359+jngong@users.noreply.github.com>
2020-06-13 14:44:15 -0400

    removed storage

--- extension/background.js
+++ extension/background.js
@@ -1,22 +1,17 @@
 'use strict';
 
-// Kept this code for reference, although I don't think we need storage.
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
-    console.log(`The color is green.`);
-  });
-
+chrome.runtime.onInstalled.addListener(function() {
   // Show the page action (popup.html) when the URL host demain contains amazon
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostContains: 'amazon.com' },
-          }),
+            pageUrl: { hostContains: 'amazon.com' }
+          })
         ],
-        actions: [new chrome.declarativeContent.ShowPageAction()],
-      },
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
     ]);
   });
 });
