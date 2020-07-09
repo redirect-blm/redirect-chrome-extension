37e7387738ef94c075554e05ce0b026b5295810f
mjr68 <matthewrose1799@gmail.com>
2020-07-08 21:09:00 -0400

    removed ethics card from frontend view

--- src/popup/pages/Root.js
+++ src/popup/pages/Root.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import Header from '../components/Header/Header';
 import BusinessList from '../components/BusinessList/BusinessList';
-import EthicsCard from '../components/EthicsCard/EthicsCard';
+// import EthicsCard from '../components/EthicsCard/EthicsCard';
 import './root.module.css';
 
 class Root extends Component {
@@ -15,7 +15,7 @@ class Root extends Component {
     return (
       <div>
         <Header />
-        <EthicsCard domContent={domContent} />
+        {/* <EthicsCard domContent={domContent} /> */}
         <BusinessList domContent={domContent} />
       </div>
     );
